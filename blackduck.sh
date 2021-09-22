d830591bc45505ad08ce50d26aaaf726efea4dcc
David Schuppa <d.schuppa@gmail.com>
2021-09-22 15:42:05 +0200

    háááááp

--- blackduck.sh
+++ blackduck.sh
@@ -4,13 +4,13 @@ chmod +x detect.sh
 --blackduck.timeout=6000 \
 --blackduck.trust.cert=true \
 --detect.blackduck.signature.scanner.memory=4096 \
---detect.blackduck.signature.scanner.paths=~/work/android/android-emarsys-sdk/ \
+--detect.blackduck.signature.scanner.paths=~/work/android-emarsys-sdk/android-emarsys-sdk \
 --detect.code.location.name="https://github.com/emartech/android-emarsys-sdk" \
 --detect.project.name="Github:emartech / android-emarsys-sdk" \
 --detect.project.version.name=android-emarsys-sdk \
 --detect.report.timeout=4800 \
---detect.source.path=~/work/android/android-emarsys-sdk/  \
+--detect.source.path=~/work/android-emarsys-sdk/android-emarsys-sdk  \
 --detect.latest.release.version=6.7.0 \
 --detect.npm.include.dev.dependencies=false \
 --detect.sbt.report.search.depth=4 \
---detect.gradle.path=~/work/android/android-emarsys-sdk/gradlew
+--detect.gradle.path=~/work/android-emarsys-sdk/android-emarsys-sdk/gradlew
